5935052810af240af99ce03268b9cb44bd442cf0
Max2025M <massinguecarlosantonio+2025@gmail.com>
2025-10-29 22:24:14 +0200

    Delete app/js directory

--- app/js/termux.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.smsSend = function (options, success, error) {
-  exec(success, error, 'TermuxApi', 'smsSend', [options]);
-};
-
-exports.telephonyInfo = function (success, error) {
-  exec(success, error, 'TermuxApi', 'telephonyInfo', []);
-};
